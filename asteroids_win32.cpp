d34730ee930bcac9d0ad5c43b7a5cc8257971912
Terrence O'Donovan <terrence.odonovan@gmail.com>
2017-06-21 20:09:16 -0400

    Basic window message handling, including basic drawing via WM_PAINT/FillRect.

--- asteroids_win32.cpp
+++ asteroids_win32.cpp
@@ -1,5 +1,8 @@
 #include <windows.h>
 
+/* The main callback for our window. This function will handle all
+   messages passed from Windows. The default case must be maintained
+   to ensure that messages not explicitly handled are handled by Windows. */
 LRESULT CALLBACK AsteroidsWindowCallback(HWND WindHandle,
                                          UINT Message,
                                          WPARAM wParam,
@@ -7,8 +10,53 @@ LRESULT CALLBACK AsteroidsWindowCallback(HWND WindHandle,
 {
     LRESULT Result = 0;
 
-    switch(Message)
+    switch (Message)
     {
+        // To do: handle all painting, resize, etc.
+        case WM_ACTIVATEAPP:
+        {
+            OutputDebugString("Activate app");
+            return 0;            
+        }
+        case WM_PAINT:
+        {
+            static int Color = 0;
+
+            PAINTSTRUCT PaintStruct;
+            RECT ClientRect;
+            HBRUSH Brush;
+            if (Color == 0)
+            {
+                Brush = (HBRUSH)GetStockObject(BLACK_BRUSH);
+                Color = 1;
+            }
+            else
+            {
+                Brush = (HBRUSH)GetStockObject(WHITE_BRUSH);
+                Color = 0;
+            }
+            HDC PaintDC = BeginPaint(WindHandle, &PaintStruct);
+            GetClientRect(WindHandle, &ClientRect);
+            FillRect(PaintDC, &ClientRect, Brush);
+            EndPaint(WindHandle, &PaintStruct);
+            return 0;
+        }
+        case WM_KEYDOWN:
+        {
+            switch (wParam)
+            {
+                case VK_UP:
+                {
+                    OutputDebugString("Up arrow");
+                    return 0;
+                }
+                default:
+                {
+                    OutputDebugString("Other key");
+                    return 0;
+                }
+            }
+        }
         default:
         {
             Result = DefWindowProc(WindHandle, Message, wParam, lParam);
@@ -18,17 +66,19 @@ LRESULT CALLBACK AsteroidsWindowCallback(HWND WindHandle,
     return Result;
 }
 
+/* The graphical entry point for Windows. */
 int CALLBACK WinMain(HINSTANCE Instance,
                      HINSTANCE PrevInstance,
                      LPSTR CommandLine,
                      int CommandShow)
 {
-    WNDCLASS WindowClass = {};
-    WindowClass.style = CS_CLASSDC | CS_OWNDC | CS_HREDRAW | CS_VREDRAW;
+    WNDCLASS WindowClass = {}; // Ensure the struct is zeroed.
+    WindowClass.style = CS_OWNDC | CS_HREDRAW | CS_VREDRAW;
     WindowClass.lpfnWndProc = &AsteroidsWindowCallback;
     WindowClass.hInstance = Instance;
     WindowClass.lpszClassName = "AsteroidsWindow";
 
+    // RegisterClass returns an ATOM, which we likely will not need to store.
     if (RegisterClass(&WindowClass))
     {
         HWND WindowHandle = CreateWindowEx(0, "AsteroidsWindow", "Asteroids", WS_OVERLAPPEDWINDOW|WS_VISIBLE,
@@ -50,7 +100,6 @@ int CALLBACK WinMain(HINSTANCE Instance,
                     break;
                 }
             }
-            UpdateWindow(WindowHandle);
         }
     }   
 
