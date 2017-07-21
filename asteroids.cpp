4bfe358a7bdd0a37a1fd45841fe602a163a65bb8
Terrence O'Donovan <terrence.odonovan@gmail.com>
2017-07-21 21:17:55 +0000

    Very minor changes during workday.

--- asteroids.cpp
+++ asteroids.cpp
@@ -45,6 +45,11 @@ struct game_object
     float AngularMomentum;
 };
 
+struct game_state
+{
+    game_object PlayerObject;
+};
+
 // Thoughts for allocating into this game memory...
 // Need to allocate a chunk of space for the asteroids somehow (even just VirtualAlloc), and assign the resulting pointer to Memory.Asteroids
 // Then, to create an asteroid, build a new asteroid within a function, then do a memcpy(), where the location is equal
@@ -98,8 +103,6 @@ void DrawLineWidth(platform_bitmap_buffer *Buffer, vec_2 *Point1, vec_2 *Point2,
         vec_2 Coords;
         Coords.X = x0;
         Coords.Y = y0;
-        // To restore the anti-aliasing, this proc would have to take a param
-        // that represented the amount to scale down the 'brightness' of the pixel.
         SetPixelInBuffer(Buffer, &Coords, Color, max(0,100*(abs(err-dx+dy)/ed-wd+1)));
         e2 = err; x2 = x0;
         if (2*e2 >= -dx)
@@ -190,7 +193,7 @@ void UpdateGameAndRender(game_memory *Memory, platform_bitmap_buffer *OffscreenB
 {
     if (!Memory->IsInitialized)
     {
-        // Initialize game memory here!
+        // Set up game memory here!
         Player = {};
         P_Model = {};
         Player.Type = PLAYER;
